c4360e8ebd40afd7628b2d30c50cc4614be5e294
omija-tea <playjnj@khu.ac.kr>
2026-02-24 17:49:34 +0900

    Initial commit

---
+++ src/test/java/com/omija/miniproject/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omija.miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
